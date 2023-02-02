72253954da9ae34d5b9f9012bb5eb4af496c242e
champuser <jude.lindale@mymail.champlain.edu>
2023-02-02 13:38:19 -0500

    week3 dns servers

---
+++ week3/portscanner2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Modified bash script.
+
+
+prefix=$1
+port=$2
+
+echo "ip,port"
+for i in $(seq 1 254); do
+    timeout .1 bash -c "echo >/dev/tcp/$prefix.$i/$port" 2>/dev/null &&
+      echo "$prefix.$i,$port"
+done
