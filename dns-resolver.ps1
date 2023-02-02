ab69d6147e6179fb7ef6dd08be1d01336f297bd9
jude-lindale <70959569+jude-lindale@users.noreply.github.com>
2023-02-02 15:11:49 -0500

    Delete dns-resolver.ps1

--- dns-resolver.ps1
+++
@@ -1,8 +0,0 @@
-param($prefix, $dns)
-
-for ($i=1; $x -le 254; $i++)
-{
-    $address = "$prefix.$i"
-    Resolve-DnsName -DnsOnly $address -Server $dns -ErrorAction Ignore | Select-Object Name, NameHost
-
-}
